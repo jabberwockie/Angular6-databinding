f20bae886ce5ae827ea6a75d40cb01276d8aca5e
jabberwockie <camila.vigneaux@gmail.com>
2018-06-22 14:06:28 -0400

    hooks

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,16 +1,39 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, Input, SimpleChange, DoCheck, AfterContentInit, OnChanges} from '@angular/core';
+import {SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements
+  OnInit,
+  DoCheck,
+  AfterContentInit,
+  OnChanges{
   @Input('srvElement') element: {type: string, name: string, content: string};
+  @Input() name: string;
 
-  constructor() { }
+  constructor() {
+    console.log('constructor called!');
+  }
 
   ngOnInit() {
+    console.log('ngOnInit called!');
   }
+
+  ngOnChanges(changes: SimpleChanges){
+    console.log('ngOnChanges called!');
+    console.log(changes);
+  }
+
+  ngDoCheck(){
+    console.log('ngDoCheck called!');
+  }
+
+  ngAfterContentInit(){
+    console.log('ngAfterContentInit called!');
+  }
+
 
 }
